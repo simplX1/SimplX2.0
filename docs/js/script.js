e98ab340f801754c1bf6ee08753c97bc4a315401
stewart-clay-simplx <stewart@simplx.co.za>
2021-04-16 07:39:04 +0200

    fixed header nav for mobile

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,11 +4,3 @@ $('nav').coreNavigation({
     responsideSlide: true
      
 });
-
-const menu_btn = document.querySelector('.hamburger');
-window.onload = function () {
-    menu_btn.addEventListener('toggle-bar', function () {
-        $(".toggle-bar core-nav-toggle").css("background", "red !important");
-    
-    });
-}
